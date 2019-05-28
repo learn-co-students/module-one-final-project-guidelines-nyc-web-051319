8aa1f6ed3cae7096a951278cf3d439a8ba499278
Will Bainton <baintonw@gmail.com>
2019-05-28 14:12:18 -0400

    added-bio-to-users-migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "episodes", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "bio"
   end
 
   create_table "views", force: :cascade do |t|
