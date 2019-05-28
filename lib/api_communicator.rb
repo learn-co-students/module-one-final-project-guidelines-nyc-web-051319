9ffa8284b314c5dbfeb378c208c9d3a17cc31d1f
Constantine Chutis <constantinec84@gmail.com>
2019-05-28 16:50:51 -0400

    Got API set up and pulling properly, and iterator set up to look at stats, saved to variable

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,15 +2,26 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-# URL = ("http://www.dnd5eapi.co/monsters/")
-def get_monster_hash
-    monster_string = RestClient.get('http://www.dnd5eapi.co/api/monsters/')
+URL = ("http://www.dnd5eapi.co/api/monsters/")
+
+# Gets to the API level info for each monster 
+def get_monster_info
+    monster_string = RestClient.get(URL)
     monster_hash = JSON.parse(monster_string)
     monster_info = monster_hash["results"]
-
-    monster_hash["results"].each do |monster|
-         
+    monsters = monster_info.map do |monster|
+        JSON.parse(RestClient.get(monster["url"]))
+        # binding.pry
     end
+    # binding.pry
+    monsters
 end
 
-get_monster_hash
+
+puts get_monster_info
+
+# def choose_monster_by_id(num)
+#     get_monster_info[num]
+# end
+
+# puts choose_monster_by_id(37)
