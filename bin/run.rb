93f8705afdc7acc51becce17048eb546fbc47b9a
chrisyuaners <chrisyuan7@gmail.com>
2019-05-29 12:08:25 -0400

    updated functions

--- bin/run.rb
+++ bin/run.rb
@@ -1,36 +1,43 @@
 require_relative '../config/environment'
 require_rel '../app'
 
-puts "===================================================================================="
-
-puts "       ----  ---       ---      -----------      ------------           ------"
-puts "    ----     ---       ---      ------------     ------------         ----  ----"
-puts "  --         ---       ---      ---        ---        --            ----       ---"
-puts "  --         ---       ---      ---        ---        --            ----"
-puts "  --         ---       ---      ---       ---         --            -----"
-puts "  --         -------------      -----------           --             -----"
-puts "  --         -------------      ----------            --                -----"
-puts "  --         -------------      ---     ---           --                  -----"
-puts "  --         ---       ---      ---      ---          --                   -----"
-puts "  ----       ---       ---      ---       ---         --          ----      -----"
-puts "   ----      ---       ---      ---        ---   ------------       ----   -----"
-puts "       ----  ---       ---      ---         ---  ------------         ------"
-
-puts "===================================================================================="
-sleep 2
+# puts "===================================================================================="
+#
+# puts "       ----  ---       ---      -----------      ------------           ------"
+# puts "    ----     ---       ---      ------------     ------------         ----  ----"
+# puts "  --         ---       ---      ---        ---        --            ----       ---"
+# puts "  --         ---       ---      ---        ---        --            ----"
+# puts "  --         ---       ---      ---       ---         --            -----"
+# puts "  --         -------------      -----------           --             -----"
+# puts "  --         -------------      ----------            --                -----"
+# puts "  --         -------------      ---     ---           --                  -----"
+# puts "  --         ---       ---      ---      ---          --                   -----"
+# puts "  ----       ---       ---      ---       ---         --          ----      -----"
+# puts "   ----      ---       ---      ---        ---   ------------       ----   -----"
+# puts "       ----  ---       ---      ---         ---  ------------         ------"
+#
+# puts "===================================================================================="
+# sleep 2
 
 puts "Please write your name"
 #need a method that finds user by name
 #if name is in database
 #puts welcome back
-traveler_name = gets.chomp
-trav = Traveler.new(name: traveler_name)
+traveler_name = gets.chomp.split.map(&:capitalize).join(" ")
+current_user = ""
+Traveler.all.each do |trav|
+  if trav.name == traveler_name
+    current_user = trav
+  end
+end
 
+if current_user == ""
+  current_user = Traveler.create(name: traveler_name)
+end
 
-
-puts "Hey #{traveler_name}, what would you like to do?"
+puts "Hey #{current_user.name}, what would you like to do?"
 puts ""
-sleep 1
+sleep 0.5
 puts "Create new plan ('new plan'), view plan ('view plan'), update plan ('update plan'), remove activity/plan ('remove')"
 user_option = gets.chomp.downcase
 # binding.pry
@@ -42,19 +49,20 @@ case user_option
     sleep 1
       Activity.all.map {|activity| puts activity.name}
       puts "---------------------------------------------------"
-      activity_name = gets.chomp.downcase
-      Activity.all.each do |act|
-        if activity_name == act.name
-          trav.plans << act
-        end
-      end
-      puts trav.plans
+      activity_name = gets.chomp.split.map(&:capitalize).join(" ")
+      new_plan = Plan.new
+
+      new_plan.date = Time.now.strftime('%y/%m/%d')
+      Activity.find_by(name: "#{activity_name}").plans << new_plan
+      current_user.plans << new_plan
+      binding.pry
   when "update plan"
-    puts "The tank is almost empty. Quickly, find a gas station!"
+    puts "Which plan would you like to update?"
+    current_user.plans.all.map {|plan| puts "#{plan.activity} - #{plan.date}"}
   when "view plan"
-    trav.view_plans
+    current_user.view_plans
   when "remove"
     puts "The tank is almost full."
   else
-    puts "Sorry #{traveler_name}, thats not an option. Please choose one of the following options."
+    puts "Sorry #{current_user.name}, thats not an option. Please choose one of the following options."
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Traveler.destroy_all
+Plan.destroy_all
+Activity.destroy_all
+Location.destroy_all
+
 #Activity data
 Activity.create(name: "Six Flags", description: "Amusement Park", rating: 2, time_open: '09:00:00', time_close: '21:00:00')
 Activity.create(name: "Madison Square Garden", description: "Sports Arena/Music Venue", rating: 5, time_open: '16:00:00', time_close: '00:00:00')
@@ -8,7 +13,7 @@ Activity.create(name: "Long Island Skydiving Center", description: "Skydiving", 
 Activity.create(name: "Brookfield Place", description: "Shopping Mall", rating: 2, time_open: '10:00:00', time_close: '20:00:00')
 Activity.create(name: "Brooklyn Botanic Garden", description: "52-acre Garden", rating: 3, time_open: '08:00:00', time_close: '18:00:00')
 Activity.create(name: "Bryant Park", description: "Public NYC Park", rating: 2, time_open: '07:00:00', time_close: '22:00:00')
-Activity.create(name: "Chuck e Cheese", description: "Arcade/Restaurant", rating: 3, time_open: '11:00:00', time_close: '22:00:00')
+Activity.create(name: "Chuck E Cheese", description: "Arcade/Restaurant", rating: 3, time_open: '11:00:00', time_close: '22:00:00')
 
 #Traveler data
 Traveler.create(name: "Chris")
