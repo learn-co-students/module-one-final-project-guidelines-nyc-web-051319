838ebdfb5ac963433a1dae63699cfcec9582e7f8
Constantine Chutis <constantinec84@gmail.com>
2019-05-29 13:57:17 -0400

    Finished up status and levelup, working on Attack logic

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,15 +1,17 @@
 # Create Adventurer with Name, Class, HP, WeaponDMG, Alive? Boolean
+require 'pry'
 class Player < ActiveRecord::Base
     has_many :dungeons, through: :dungeon_crawls
 
     # Attack, Level Up, Status, BattleCry, Min & Max Damage, Attack Accuracy
 
     # View all current stats of a character
-    def status
+    def print_status
+        puts "LEVEL #{self.level}"
         puts "Health: #{self.current_hp}/#{self.max_hp}"
         puts "Damage: #{self.min_dmg} to #{self.max_dmg} DMG per attack."
         puts "Accuracy Modifer: #{self.accuracy}% to hit."
-        puts "#{self.battlecry}"
+        puts "#{self.battlecry}!!!"
     end
     # Rest function resets current health to max health. Will be called when leveled up. 
     def rest
@@ -17,7 +19,8 @@ class Player < ActiveRecord::Base
     end
     # Basic attack logic. % Chance to hit (Accuracy Stat), random damage within range if successful (min -> max dmg)
     def attack
-        self.dmg - monster.hp
+        damage = current_monster.hp - rand(self.min_dmg..self.max_dmg)
+        damage
     end
     # Battle option for potential for monster to miss a turn
     def intimidate
@@ -27,11 +30,15 @@ class Player < ActiveRecord::Base
     end
     # Every successful run will increase level and up stats
     def level_up
-        self.max_hp + rand(50..75) = self.max_hp
+        self.max_hp = self.max_hp + rand(50..75)
         rest
-        self.min_dmg * 1.2 = self.min_dmg
-        self.max_dmg * 1.4 = self.max_dmg
-        self.accuracy + rand[]
+        self.min_dmg = self.min_dmg * 1.2
+        self.max_dmg = self.max_dmg * 1.4
+        self.accuracy += 2
+        self.level += 1
     end
+
+    binding.pry
+    0
 
 end
