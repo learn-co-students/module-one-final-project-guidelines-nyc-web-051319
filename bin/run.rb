86f3db3b9afd7afecbcad74be62a0f1d601d6e10
Aaron Smith <smitham50@gmail.com>
2019-05-29 15:09:35 -0400

    Added weapon to create_players and players table

--- bin/run.rb
+++ bin/run.rb
@@ -2,11 +2,15 @@
 require 'pry'
 require_relative '../config/environment'
 
+def title
+  #displays title screen
 puts "*" * 60
 puts "\t\tWorking Title!"
 puts "*" * 60
+end
 
 def greeting
+  #greeting and menu prompt
 puts "Greetings, Adventurer! What would you like to do?"
 puts "\s\s\s1. Create Character"
 puts "\s\s\s2. Load Character"
@@ -16,29 +20,39 @@ end
 
 
 def create_player(name_input, battlecry_input)
-  Player.create(:name => name_input, :max_hp => 200, :current_hp => 200, :min_dmg => 6, :max_dmg => 12, :alive => true, :level => 1, :battlecry => battlecry_input, :accuracy => 50)
+  #instantiates a new Player and adds them to ActiveRecord database
+  Player.create(:name => name_input, :max_hp => 200, :current_hp => 200, :min_dmg => 6, :max_dmg => 12, :alive => true, :level => 1, :battlecry => battlecry_input, :accuracy => 50, :weapon = weapon_input)
+  player(Player.last)
 end
 
 def character_select(user_input)
+  #allows player to create new character, pick an existent one, or reloads if they fail to do either -- choice leads to dungeon selection menu
   if user_input == "1"
     puts "Please enter a name"
     name_input = gets.chomp
     puts "Please enter a battlecry"
     battlecry_input = gets.chomp
-    create_player(name_input, battlecry_input)
+    puts "Please enter a melee weapon"
+    weapon_input = gets.chomp
+    create_player(name_input, battlecry_input, weapon_input)
     choose_dungeon
   elsif user_input == "2"
     puts "Please type the name of the character you want to play as"
     Player.all.each_with_index do |player, n|
-      puts "\t#{player.name}"
+      puts "\t#{n + 1}. #{player.name}"
     end
     character_selection = gets.chomp
-    puts "You picked #{Player(character_selection)}!"
+    puts "You picked #{Player.find_by_name(character_selection).name}!"
     choose_dungeon
   elsif user_input != "1" && user_input != "2"
     puts "That is not a valid command. Stop it."
     greeting
   end
+end
+
+def current_player(player_selection)
+  player = player_selection
+  player
 end
 
 def choose_dungeon
@@ -51,10 +65,5 @@ def choose_dungeon
   #method instantiate dungeon crawl
 end
 
-# if character_list[character_selection]
-#   character_list[character_selection]
-# else
-#   puts "That character doesn't exist. Please try again."
-# end
-
+title
 greeting
