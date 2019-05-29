3c601a7cd59218923666f2b5ea60ec8b8af5bda9
Constantine Chutis <constantinec84@gmail.com>
2019-05-29 11:54:19 -0400

    Notated player class, fleshed out Player Class Methods

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,28 +2,30 @@
 class Player < ActiveRecord::Base
     has_many :dungeons, through: :dungeon_crawls
 
-    # Attack, Level Up, Status, BattleCry, Min & Max Damage
+    # Attack, Level Up, Status, BattleCry, Min & Max Damage, Attack Accuracy
+
+    # View all current stats of a character
     def status
         puts "Health: #{self.current_hp}/#{self.max_hp}"
         puts "Damage: #{self.min_dmg} to #{self.max_dmg} DMG per attack."
         puts "Accuracy Modifer: #{self.accuracy}% to hit."
         puts "#{self.battlecry}"
     end
-    
+    # Rest function resets current health to max health. Will be called when leveled up. 
     def rest
         self.current_hp = self.max_hp
     end
-
+    # Basic attack logic. % Chance to hit (Accuracy Stat), random damage within range if successful (min -> max dmg)
     def attack
         self.dmg - monster.hp
     end
-
+    # Battle option for potential for monster to miss a turn
     def intimidate
         puts "You roar with beastial vigor!"
         sleep(2)
         puts "#{self.battlecry}!!!"
     end
-
+    # Every successful run will increase level and up stats
     def level_up
         self.max_hp + rand(50..75) = self.max_hp
         rest
