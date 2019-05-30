e869e33b045d31b99a3873736690aeda023e0404
locky5 <daikevin19@yahoo.com>
2019-05-30 12:44:34 -0400

    removing useless comments

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,22 +2,8 @@ class Commit < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
-  # def initialize(attributes)
-  #   self.on_time = attributes[:commit_date] < self.project.
-  #   if self.commit_date > self.project.due_date # if commit date is past due date
-  #     self.on_time = false
-  #     self.save
-  #   end
-  # end
-
-  def on_time? # takes a project instance as an argument
-    self.commit_date <= self.project.due_date # if commit date is past due date
-    #   self.on_time = false
-    #   self.save
-    #   "You're late!"
-    # else
-    #   "You're on time."
-    # end
+  def on_time?
+    self.commit_date <= self.project.due_date
   end
 
   def delete_commit
