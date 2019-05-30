b8e501a11cfc5b248225373d28df353e6166042c
Aaron Smith <smitham50@gmail.com>
2019-05-29 21:56:08 -0400

    Added line breaks to menu selections

--- app/models/CLI.rb
+++ app/models/CLI.rb
@@ -14,6 +14,7 @@ class CLI < ActiveRecord::Base
     puts "   1. Create Character"
     puts "   2. Load Character"
     user_input = gets.chomp
+    puts " "
     character_select(user_input)
   end
 
@@ -22,6 +23,7 @@ class CLI < ActiveRecord::Base
     @current_player = Player.create(name: name_input, max_hp: 200, current_hp: 200, min_dmg: 6, max_dmg: 12, alive: true, level: 1, battlecry: battlecry_input, accuracy: 50, weapon: weapon_input)
     puts "Okay, #{@current_player.name}. Ready yourself..."
     sleep(2)
+    puts " "
   end
 
   #allows player to create new character, pick an existent one, or reloads if they fail to do either -- choice leads to dungeon selection menu
@@ -29,10 +31,13 @@ class CLI < ActiveRecord::Base
     if user_input == "1"
       puts "Please enter a name:"
       name_input = gets.chomp
+      puts " "
       puts "Please enter a battlecry:"
       battlecry_input = gets.chomp
+      puts " "
       puts "Please enter a melee weapon:"
       weapon_input = gets.chomp
+      puts " "
       self.create_player(name_input, battlecry_input, weapon_input)
       choose_dungeon
     elsif user_input == "2"
@@ -45,10 +50,12 @@ class CLI < ActiveRecord::Base
       character_selection = gets.chomp.to_i
       puts "Okay, #{Player.find_by_name(player_array[character_selection - 1]).name}. You know the drill..."
       sleep(2)
+      puts " "
       @current_player = Player.find_by_name(player_array[character_selection - 1])
       self.choose_dungeon
     elsif user_input != "1" && user_input != "2"
       puts "That is not a valid command. Stop it."
+      puts " "
       self.greeting
     end
   end
@@ -56,6 +63,7 @@ class CLI < ActiveRecord::Base
   #chooses a dungeon and calls method to instantiate dungeon crawl
   def self.choose_dungeon
     puts "Which dungeon do you dare to enter?"
+    puts " "
     Dungeon.all.each_with_index do |dungeon, n|
       puts "   #{n + 1}. #{dungeon.name} -- #{dungeon.difficulty}"
     end
