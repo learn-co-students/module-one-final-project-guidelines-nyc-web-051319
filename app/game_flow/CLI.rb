63168e8691d27b930aa94f6bec6f54a70f74e970
Constantine Chutis <constantinec84@gmail.com>
2019-05-31 01:31:59 -0400

    Added Catpix Gemfile, and loaded small images to be used at beginging of each dungeon

--- app/game_flow/CLI.rb
+++ app/game_flow/CLI.rb
@@ -6,6 +6,7 @@ class CLI
   #displays title screen
   def self.title
     SFX.title_music
+    catpix(app/images/dragon.png)
     title = "Legend of Rubyerion: The Seven Paths"
     puts " "
     puts "*" * 60
